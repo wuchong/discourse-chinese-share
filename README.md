ecef8984b2295abee8c3705c7080b3183de0efc2
wuchong <cool1014@qq.com>
2014-12-01 11:37:16 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-discourse-chinese-share
+Discourse Chinese Share
 =======================
 
-Easy plugin to add Chinese share buttons in Discourse
+A Easy plugin to add Chinese share buttons in [Discourse](https://github.com/discourse/discourse).
+
+We can add share buttons like Weibo, Wechat, Renren.
+
+## 在 Docker 镜像上的安装方法
+
+修改你的 `/var/discourse/containers/app.yml` 文件. 默认的 `app.yml` 有一个 `hooks` 属性，如下:
+
+```ruby
+hooks:
+  after_code:
+    - exec:
+        cd: $home/plugins
+        cmd:
+          - mkdir -p plugins
+          - git clone https://github.com/discourse/docker_manager.git
+```
+在那后面添加一行，注意空格对齐。
+```
+- git clone https://github.com/wuchong/discourse-chinese-share.git
+```
+
+重建 Discourse：`/var/discourse/launcher rebuild app`
+
+进入管理员页面 -> 设置 -> 基本设置 -> 找到`share links`配置项，加入你想添加的分享按钮，如`weibo`,`wechat`,`renren`。
+
+刷新页面，完成。
+
+![]()
--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -11,13 +11,23 @@
   Discourse.ShareLink.addTarget('wechat', {
     iconClass: 'fa-wechat jiathis_button_weixin',
     generateUrl: function(link) {
-    },
-    shouldOpenInPopup: false
+      var $this = $('.share');
+      var html = [
+        '<a href="#" class="overlay" id="qrcode"></a>',
+        '<div class="qrcode clearfix"><span>扫描二维码分享到微信朋友圈</span><a class="qrclose" href="#nothing"></a><strong>Loading...Please wait</strong><img id="qrcode-pic" data-src="http://s.jiathis.com/qrcode.php?url=' + encodedUrl + '"/></div>'
+        ].join('');
+      $this.append(html);
+      $('.article-share-qrcode').click(function(){
+        var imgSrc = $('#qrcode-pic').attr('data-src');
+        $('#qrcode-pic').attr('src', imgSrc);
+        $('#qrcode-pic').load(function(){
+            $('.qrcode strong').text(' ');
+        });
+        },
+      },
+      shouldOpenInPopup: false
   });
 
-  document.write('<' + 'script language="javascript" type="text/javascript" charset="utf-8"');
-  document.write(' src="http://v3.jiathis.com/code/jia.js?uid=1">');
-  document.write('<'+'/script'+'>');
 
   Discourse.ShareLink.addTarget('renren', {
     iconClass: 'fa-renren',
