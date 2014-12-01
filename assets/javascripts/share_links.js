34305dc6b24dff2e9e5aba960489c34254cd41ff
wuchong <cool1014@qq.com>
2014-12-01 12:59:25 +0800

    update

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -9,23 +9,12 @@
   });
 
   Discourse.ShareLink.addTarget('wechat', {
-    iconClass: 'fa-wechat jiathis_button_weixin',
+    iconClass: 'fa-wechat',
     generateUrl: function(link) {
-      var $this = $('.share');
-      var html = [
-        '<a href="#" class="overlay" id="qrcode"></a>',
-        '<div class="qrcode clearfix"><span>扫描二维码分享到微信朋友圈</span><a class="qrclose" href="#nothing"></a><strong>Loading...Please wait</strong><img id="qrcode-pic" data-src="http://s.jiathis.com/qrcode.php?url=' + encodeURIComponent(link) + '"/></div>'
-        ].join('');
-      $this.append(html);
-      $('.article-share-qrcode').click(function(){
-        var imgSrc = $('#qrcode-pic').attr('data-src');
-        $('#qrcode-pic').attr('src', imgSrc);
-        $('#qrcode-pic').load(function(){
-            $('.qrcode strong').text(' ');
-        });
-        },
-      },
-      shouldOpenInPopup: false
+      return ("http://s.jiathis.com/qrcode.php?url=" + encodeURIComponent(link));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 160
   });
 
 
