2f8d32d9e0737dd9d5650a7d544ef5664fc7f484
wuchong <cool1014@qq.com>
2014-12-01 13:30:04 +0800

    update

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -9,12 +9,23 @@
   });
 
   Discourse.ShareLink.addTarget('wechat', {
-    iconClass: 'fa-wechat',
+    iconClass: 'fa-wechat jiathis_button_weixin',
     generateUrl: function(link) {
-      return ("http://s.jiathis.com/qrcode.php?url=" + encodeURIComponent(link));
-    },
-    shouldOpenInPopup: true,
-    popupHeight: 160
+      var $this = $('.share');
+      var html = [
+        '<a href="#" class="overlay" id="qrcode"></a>',
+        '<div class="qrcode clearfix"><span>扫描二维码分享到微信朋友圈</span><a class="qrclose" href="#nothing"></a><strong>Loading...Please wait</strong><img id="qrcode-pic" data-src="http://s.jiathis.com/qrcode.php?url=' + encodeURIComponent(link) + '"/></div>'
+        ].join('');
+      $this.append(html);
+      $('.article-share-qrcode').click(function(){
+        var imgSrc = $('#qrcode-pic').attr('data-src');
+        $('#qrcode-pic').attr('src', imgSrc);
+        $('#qrcode-pic').load(function(){
+            $('.qrcode strong').text(' ');
+        });
+        });
+      },
+      shouldOpenInPopup: false
   });
 
 
@@ -24,7 +35,7 @@
       return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
     },
     shouldOpenInPopup: true,
-    popupHeight: 628
+    popupHeight: 465
   });
 
 })();
