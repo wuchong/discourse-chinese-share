8681ba530196b93f097020a3d4d1c251e21e4695
unknown <cool1014@qq.com>
2014-12-31 16:04:57 +0800

    add site name in title

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -2,29 +2,27 @@
   Discourse.ShareLink.addTarget('weibo', {
     iconClass: 'fa-weibo',
     generateUrl: function(link, title) {
-      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title));
+      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title)) + "&searchPic=true&style=number";
     },
     shouldOpenInPopup: true,
     popupHeight: 370
   });
 
   Discourse.ShareLink.addTarget('wechat', {
-    iconClass: 'fa-wechat',
+    iconClass: 'fa-wechat jiathis_button_weixin',
     generateUrl: function(link) {
-      return ("http://s.jiathis.com/qrcode.php?url=" + encodeURIComponent(link));
     },
-    shouldOpenInPopup: true,
-    popupHeight: 200
+    shouldOpenInPopup: false
   });
 
 
   Discourse.ShareLink.addTarget('renren', {
     iconClass: 'fa-renren',
     generateUrl: function(link, title) {
-      return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
+      return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
     },
     shouldOpenInPopup: true,
-    popupHeight: 465
+    popupHeight: 628
   });
 
 })();
