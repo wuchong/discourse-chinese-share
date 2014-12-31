d15d1e12784529c960dea39a158087ed68e1ecf0
unknown <cool1014@qq.com>
2014-12-31 16:17:29 +0800

    restore wechat share

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -2,19 +2,24 @@
   Discourse.ShareLink.addTarget('weibo', {
     iconClass: 'fa-weibo',
     generateUrl: function(link, title) {
-      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title)) + "&searchPic=true&style=number";
+      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title))+ "&searchPic=true&style=number";
     },
     shouldOpenInPopup: true,
     popupHeight: 370
   });
 
   Discourse.ShareLink.addTarget('wechat', {
-    iconClass: 'fa-wechat jiathis_button_weixin',
+    iconClass: 'fa-wechat',
     generateUrl: function(link) {
+      return ("http://s.jiathis.com/qrcode.php?url=" + encodeURIComponent(link));
     },
-    shouldOpenInPopup: false
+    shouldOpenInPopup: true,
+    popupHeight: 200
   });
 
+  document.write('<' + 'script language="javascript" type="text/javascript" charset="utf-8"');
+  document.write(' src="http://v3.jiathis.com/code/jia.js?uid=1">');
+  document.write('<'+'/script'+'>');
 
   Discourse.ShareLink.addTarget('renren', {
     iconClass: 'fa-renren',
