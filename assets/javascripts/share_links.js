e5c36a32ec8b886e4df3ab85d1a32e4d0e92298a
wuchong <cool1014@qq.com>
2015-01-03 01:20:07 +0800

    add [] to title

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -2,7 +2,7 @@
   Discourse.ShareLink.addTarget('weibo', {
     iconClass: 'fa-weibo',
     generateUrl: function(link, title) {
-      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title))+ "&searchPic=true&style=number";
+      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=【码农圈 - " + encodeURIComponent(title))+ "】&searchPic=true&style=number";
     },
     shouldOpenInPopup: true,
     popupHeight: 370
@@ -24,7 +24,7 @@
   Discourse.ShareLink.addTarget('renren', {
     iconClass: 'fa-renren',
     generateUrl: function(link, title) {
-      return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=码农圈 - " + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
+      return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=【码农圈 - " + encodeURIComponent(title) + "】&description=" + encodeURIComponent(title));
     },
     shouldOpenInPopup: true,
     popupHeight: 628
